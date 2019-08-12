fa791ae988328e9ca05fe0cfeb1e8ad1f12aac6b
Christopher Pendergast - crp998 <crp998@uowmail.edu.au>
2019-08-12 10:30:37 +1000

    Initial Commit

---
+++ 376A1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  376A1
+//
+//  Created by Chris on 12/8/19.
+//  Copyright © 2019 BScCompSci. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
